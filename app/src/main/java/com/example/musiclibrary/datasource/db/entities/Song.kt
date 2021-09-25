91f8a586582c417ded87925c3fa419763fa27782
rahul-padmakumar <rahulp5875@gmail.com>
2021-09-25 13:43:51 +0530

    develop: Add entity annotation to song data class.

--- app/src/main/java/com/example/musiclibrary/datasource/db/entities/Song.kt
+++ app/src/main/java/com/example/musiclibrary/datasource/db/entities/Song.kt
@@ -1,8 +1,10 @@
 package com.example.musiclibrary.datasource.db.entities
 
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity
 data class Song(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "song_name") val songName: String,
