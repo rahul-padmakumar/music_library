8942c0e8600ccdb623da6f1b6495fa9c7f873853
rahul-padmakumar <rahulp5875@gmail.com>
2021-09-27 10:24:13 +0530

    develop: Check whether is user is logged in dashboard and navigate user to login.

--- app/src/main/java/com/example/musiclibrary/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/musiclibrary/ui/dashboard/DashboardFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.navigation.Navigation
 import com.example.musiclibrary.R
+import com.example.musiclibrary.datastore.UserPreferences
 
 /**
  * A simple [Fragment] subclass.
@@ -14,12 +18,32 @@ import com.example.musiclibrary.R
  */
 class DashboardFragment : Fragment() {
 
+    private val dashboardViewModel by lazy {
+        ViewModelProvider(this).get(DashboardViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        UserPreferences.getUserId(requireContext()).asLiveData().observe(
+            viewLifecycleOwner,
+            {
+                if(it == 0){
+                    Navigation.findNavController(requireActivity(), R.id.fcv_music_library)
+                        .navigate(R.id.action_dashboardFragment_to_loginFragment)
+                } else {
+                    dashboardViewModel.userId = it
+                }
+            }
+        )
     }
 
     companion object {
