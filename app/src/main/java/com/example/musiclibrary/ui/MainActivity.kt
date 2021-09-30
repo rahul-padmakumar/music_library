b182195e08e42d9f5b5c4f2ea59a052191301c70
rahul-padmakumar <rahulp5875@gmail.com>
2021-09-30 09:40:14 +0530

    develop: Move main activity to UI package.

--- app/src/main/java/com/example/musiclibrary/ui/MainActivity.kt
+++ app/src/main/java/com/example/musiclibrary/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.musiclibrary
+package com.example.musiclibrary.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.musiclibrary.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
