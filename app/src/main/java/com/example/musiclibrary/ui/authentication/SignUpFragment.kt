a0f7c19045ff9bcb0a1a0eeb4e6326cf2fce03b7
rahul-padmakumar <rahulp5875@gmail.com>
2021-10-12 11:17:18 +0530

    develop: Complete the signup flow.

--- app/src/main/java/com/example/musiclibrary/ui/authentication/SignUpFragment.kt
+++ app/src/main/java/com/example/musiclibrary/ui/authentication/SignUpFragment.kt
@@ -2,22 +2,26 @@ package com.example.musiclibrary.ui.authentication
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.musiclibrary.MusicLibraryApplication
 import com.example.musiclibrary.R
+import com.example.musiclibrary.databinding.FragmentSignUpBinding
 import com.example.musiclibrary.datasource.db.entities.Address
 import com.example.musiclibrary.models.Resource
 import com.example.musiclibrary.models.UserModel
 import com.example.musiclibrary.ui.authentication.login.LoginFragment
 import javax.inject.Inject
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
 
 
 /**
@@ -28,9 +32,14 @@ import javax.inject.Inject
 class SignUpFragment : Fragment() {
 
     private var savedStateHandle: SavedStateHandle? = null
-    private var userName: String? = null
+    private var userName: String? by Delegates.observable(""){
+            _: KProperty<*>, _: String?, newValue: String? ->
+        signUpBinding?.txtUserName?.setText(newValue)
+    }
 
     private val args: SignUpFragmentArgs by navArgs()
+
+    private var signUpBinding: FragmentSignUpBinding? = null
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -51,8 +60,8 @@ class SignUpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        userName = args.argsUserName
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        signUpBinding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return signUpBinding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -62,38 +71,49 @@ class SignUpFragment : Fragment() {
             .previousBackStackEntry?.savedStateHandle
         savedStateHandle?.set(LoginFragment.IS_USER_SIGNED_IN, false)
 
+        args.argsUserName?.run {
+            userName = this
+        }
+
         signUpViewModel.liveData.observe(
             viewLifecycleOwner,
             {
                 when(it){
                     is Resource.Success -> {
-                        Log.d("Rahul", "Sign up successful")
+                        savedStateHandle?.set(LoginFragment.IS_USER_SIGNED_IN, true)
+                        findNavController().popBackStack()
                     }
                     is Resource.Failure -> {
-                        Log.d("Rahul", "Sign up failure: ${it.errorMessage}")
+                        Toast.makeText(requireContext(), it.errorMessage, Toast.LENGTH_SHORT).show()
                     }
                     is Resource.Progress -> {
-                        Log.d("Rahul", "Sign up progress")
+                        Toast.makeText(
+                            requireContext(),
+                            "SignUp progress",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
             }
         )
 
-        signUpViewModel.validateAndSignUpUser(
-            UserModel(
-                name = userName ?: "Name0",
-                password = "123456",
-                address = Address(
-                    houseName = "ARC",
-                    streetName = "SS Road",
-                    city = "TVPM",
-                    state = "KERALA",
-                    country = "IND"
+        signUpBinding?.run {
+            btnSignup.setOnClickListener {
+                signUpViewModel.validateAndSignUpUser(
+                    UserModel(
+                        name = signUpBinding?.txtUserName?.text.toString(),
+                        password = signUpBinding?.txtPassword?.text.toString(),
+                        address = Address(
+                            houseName = signUpBinding?.txtHouseName?.text.toString(),
+                            streetName = signUpBinding?.txtStreetName?.text.toString(),
+                            city = signUpBinding?.txtCityName?.text.toString(),
+                            state = signUpBinding?.txtStateName?.text.toString(),
+                            country = signUpBinding?.txtCountryName?.text.toString()
+                        )
+                    )
                 )
-            )
-        )
-
-        Log.d("Rahul", "User name is $userName")
+            }
+        }
     }
 
     companion object {
